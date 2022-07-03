08c0b2393ac395181b87d442d79da2b774274b68
Gdist <gdst.tw@gmail.com>
2022-07-03 23:23:30 +0800

    ZZZ

--- main.py
+++ main.py
@@ -52,10 +52,19 @@ def main():
 	put_scope('display')
 
 if __name__ == '__main__':
-    import argparse
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
+	import argparse
+	from pywebio.platform.tornado_http import start_server as start_http_server
+	from pywebio import start_server as start_ws_server
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--port", type=int, default=8080)
+	parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
 	args = parser.parse_args()
 
-    start_server(main, port=args.port, websocket_ping_interval=30)
+	if args.http:
+		start_http_server(main, port=args.port)
+	else:
+		# Since some cloud server may close idle connections (such as heroku),
+		# use `websocket_ping_interval` to  keep the connection alive
+		start_ws_server(main, port=args.port, websocket_ping_interval=30)
+	#start_server(main)
